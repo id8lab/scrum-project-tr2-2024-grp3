9057d4add782d87ccddb2fd60f86efe97027d538
Andrew <139748544+AndrewLim8140@users.noreply.github.com>
2024-06-12 14:44:23 +0800

    isolated sprites
    obtain and separate sprites from spritesheet

--- gameplay_test.py
+++ gameplay_test.py
@@ -1,23 +1,31 @@
 import pygame
+from sheet_to_sprite import spritesheet
 from pygame_starter import Game
-
+    
 class MyGame(Game):
     def __init__(self):
         super().__init__()
-        # Load the background image
+        # directories
+        sheet2sprite = spritesheet("assets/player_sprite.png")
+
+        # load background
         self.background = pygame.image.load("assets/bg.jpg").convert()
         self.bg_width = self.background.get_width()
         self.bg_height = self.background.get_height()
-        
-        # Load the player sprite image
-        self.img = pygame.image.load("assets/player_sprites.png").convert_alpha()
-        
+
+        # defining spritesheets
+        self.image = sheet2sprite.image_at((0,0,39,57))
+        images = []
+        self.images = sheet2sprite.images_at(((0,0,39,57),(40,0,39,57),(0,58,39,57),(40,58,39,57)))
+        # , colorkey=(255,255,255)
+
         # Initialize background positions
         self.bg_y1 = 0
         self.bg_y2 = -self.bg_height
         self.bg_scroll_speed = 5  # Adjust scroll speed as needed
-
+        
     def game(self):
+        """ Back Ground """
         # Update background positions
         self.bg_y1 += self.bg_scroll_speed
         self.bg_y2 += self.bg_scroll_speed
@@ -31,36 +39,47 @@ class MyGame(Game):
         # Draw the background images
         self.screen.blit(self.background, (0, self.bg_y1))
         self.screen.blit(self.background, (0, self.bg_y2))
-
-        # Draw the player sprite in the middle of the screen
+        
+        
+        # red highlight
+        red1 = self.images[1]    
         self.screen.blit(
-            self.img,
+            red1,
             (
-                self.screen_size[0] / 2 - self.img.get_size()[0] / 2,
-                self.screen_size[1] / 2 - self.img.get_size()[1] / 2,
+                self.screen_size[0]  - red1.get_size()[0],
+                self.screen_size[1]  - red1.get_size()[1],
             ),
         )
+        # red none
+        red0 = self.images[0]
+        self.screen.blit(
+            self.images[0],
+            (
+                self.screen_size[0] / 2 - red0.get_size()[0],
+                self.screen_size[1] / 2 - red0.get_size()[1],
+            ),
+        )
+        # blue none
+        blue0 = self.images[2]
+        self.screen.blit(
+            self.images[2],
+            (
+                self.screen_size[0]  - blue0.get_size()[0],
+                self.screen_size[1]/2  - blue0.get_size()[1],
+            ),
+        )
+
+        #blue highlight
+        blue1 = self.images[3]
+        self.screen.blit(
+            self.images[3],
+            (
+                self.screen_size[0] / 2 - blue1.get_size()[0],
+                self.screen_size[1]  - blue1.get_size()[1],
+            ),
+        )
+
 
 if __name__ == "__main__":
     game = MyGame()
     game.run()
-
-
-
-
-##class Block(pygame.sprite.Sprite):
-##
-##    # Constructor. Pass in the color of the block,
-##    # and its x and y position
-##    def __init__(self, color, width, height):
-##       # Call the parent class (Sprite) constructor
-##       pygame.sprite.Sprite.__init__(self)
-##
-##       # Create an image of the block, and fill it with a color.
-##       # This could also be an image loaded from the disk.
-##       self.image = pygame.Surface([width, height])
-##       self.image.fill(color)
-##
-##       # Fetch the rectangle object that has the dimensions of the image
-##       # Update the position of this object by setting the values of rect.x and rect.y
-##       self.rect = self.image.get_rect()
