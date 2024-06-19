7ea2d6d60390285acaa464fcdc1388ebdca3698c
Andrew <139748544+AndrewLim8140@users.noreply.github.com>
2024-06-19 14:40:35 +0800

    added player 1 control
    players could now control their respective ship simultaneously.
    player 1 : wasd
    player 2 : arrowkey

--- gameplay_test.py
+++ gameplay_test.py
@@ -15,33 +15,36 @@ class MyGame(Game):
         p2_idle = spritesheet("assets/p2_idle.png")
         p2_forward = spritesheet("assets/p2_move_highlighted.png")
 
-        test = pygame.image.load("assets/p2_move_highlighted.png")
+        p1_idle_sheet = pygame.image.load("assets/p2_idle.png")
+        p2_fwd_sheet = pygame.image.load("assets/p2_move_highlighted.png")
         
         # defines
         global win , p1_idle_sprites , p2_idle_sprites , p2_forward_sprites , p1_idle_x
-        self.p2_animation_frame = 1
+        self.p1_animation_frame , self.p2_animation_frame = 1, 1
         win = pygame.display.set_mode((1450,750))
 
         coords = []
         self.coord_x , self.coord_y = 0,0
-        img_x , img_y = 0,0
+        #img_x , img_y = 0,0
         rowct = 1
         
         # sprite defines
         self.df_sprite_size = [39 , 57] #default size of sprites
         
-        self.img_x = test.get_width()
-        self.img_y = test.get_height()
+        self.p2_fwd_img_x = p2_fwd_sheet.get_width()
+        self.p1_idle_img_x = p1_idle_sheet.get_height()
 
         """sprites per sheets"""
-        df_p1_idle_num = 2 
-        df_p2_idle_num = 2
+        self.df_p1_idle_num = 2 
+        self.df_p2_idle_num = 2
 
-        df_p1_fwd_num = 5
+        self.df_p1_fwd_num = 5
         self.df_p2_fwd_num = 5
 
         """ sprites characteristics """
         self.p2_x , self.p2_y = 725,500
+        self.p1_x , self.p1_y = 695,500
+        
         # Animation indexs
         p1_i = 'p1_idle_sprites'
         p1_a = 'p1_atk_sprites'
@@ -71,13 +74,23 @@ class MyGame(Game):
         self.bg_scroll_speed = 5  # Adjust scroll speed as needed
 
     def automaticSprites(self,target):
+        self.coord_x = 0
+        self.coord_y = 0
+        if target == 'p2_fwd_sprites':
+            sheet_size = self.p2_fwd_img_x
+            sprite_count = self.df_p2_fwd_num
+        elif target == 'p1_idle_sprites':
+            sheet_size = self.p1_idle_img_x
+            sprite_count = self.df_p1_idle_num
+
+            
         name = target+'_coords'
         globals()[name] = []
         globals()[name].append(( self.coord_x , self.coord_y , self.df_sprite_size[0] , self.df_sprite_size[1] )) # append 0,0
-
-        while len(globals()[name]) <= self.df_p2_fwd_num-1: # incremental
+        
+        while len(globals()[name]) <= sprite_count-1: # incremental
             """ x increment """
-            if (self.coord_x+ self.df_sprite_size[0] < self.img_x):
+            if (self.coord_x+ self.df_sprite_size[0] < sheet_size):
                 self.coord_x = self.coord_x + self.df_sprite_size[0]
             else :
                 self.coord_x = 0
@@ -85,11 +98,31 @@ class MyGame(Game):
                 self.coord_y = self.coord_y + self.df_sprite_size[1]
                 
             globals()[name].append(( self.coord_x , self.coord_y  , self.df_sprite_size[0] , self.df_sprite_size[1] ))
+
+        print(globals()[name])
         return  globals()[name]
             
-    def updateGameWindow(self,pl_x,pl_y):
-        curr_x = pl_x
-        curr_y = pl_y
+    def updateP1(self,p1_x,p1_y):
+        curr_x = p1_x
+        curr_y = p1_y
+        
+        try:
+            if self.p1_animation_frame + 1 >= 2:
+                self.p1_animation_frame = 1
+        except NameError:
+            self.p1_animation_frame = 0
+
+        p1 = self.p1_idle_sprites
+        print('s',self.p1_animation_frame)
+        p1_sprite = pygame.transform.scale(p1[self.p1_animation_frame],(45.5,66.5))
+        win.blit(p1_sprite,(curr_x,curr_y))
+        self.p1_animation_frame += 1
+
+        pygame.display.update
+
+    def updateP2(self,p2_x,p2_y):
+        curr_x = p2_x
+        curr_y = p2_y
         
         try:
             if self.p2_animation_frame + 1 >= 5:
@@ -122,9 +155,11 @@ class MyGame(Game):
         # Draw the background images
         self.screen.blit(self.background, (0, self.bg_y1))
         self.screen.blit(self.background, (0, self.bg_y2))
-        
+
+        # Acquire keypress
         pygame.key.set_repeat(100,200)
         keypress = pygame.key.get_pressed()
+        """Player 2 movement"""
         if keypress[K_LEFT]:
             self.p2_x -= 10
         if keypress[K_RIGHT]:
@@ -134,8 +169,19 @@ class MyGame(Game):
         if keypress[K_DOWN]:
             self.p2_y += 5
 
+        """ Player 1 movement"""
+        if keypress[K_a]:
+            self.p1_x -= 10
+        if keypress[K_d]:
+            self.p1_x += 10
+        if keypress[K_w]:
+            self.p1_y -= 5
+        if keypress[K_s]:
+            self.p1_y += 5
+
         # Update sprites
-        self.updateGameWindow(self.p2_x , self.p2_y)
+        self.updateP1(self.p1_x , self.p1_y)
+        self.updateP2(self.p2_x , self.p2_y)
       
 
 
