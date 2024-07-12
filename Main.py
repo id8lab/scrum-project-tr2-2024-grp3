e8877af1db067aea1e928392ca7fd49cce79697e
Andrew <139748544+AndrewLim8140@users.noreply.github.com>
2024-07-12 13:30:44 +0800

    refactored main into smaller classes
    split main into smaller sub classses to reduce lag

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-from SpriteLibrary import sprite_library,Player_1,Player_2
+from SpriteLibrary import sprite_library
+from Players import Player_1,Player_2
 from pygame.locals import *
 import pygame
 from pygame_starter import Game
@@ -7,7 +8,7 @@ print('hi')
 """ Setting Window's Size """
 global win
 win = pygame.display.set_mode((1450,750))
-#sprite_library()
+sl = sprite_library()
 p1 = Player_1()
 p2 = Player_2()
 
@@ -19,6 +20,9 @@ class Galaga(Game):
         win = pygame.display.set_mode((1450,750))
 
         self.clock = pygame.time.Clock()
+        self.global_tick = 0
+        self.p1_cd = 0
+        self.p2_cd = 0
         
         # Load Background
         self.background = pygame.image.load("assets/bg.jpg").convert()
@@ -31,11 +35,15 @@ class Galaga(Game):
 
         self.p1_coords = p1.GetCoords()
         self.p2_coords = p2.GetCoords()
+        self.p1_projectile = p1.GetProjectile()
+        self.p2_projectile = p2.GetProjectile()
 
     def game(self):
-        self.clock.tick(128)
-            
-        # Back Ground 
+        self.clock.tick(250)
+        self.global_tick = pygame.time.get_ticks()
+        
+        
+            # Back Ground 
         ''' Update background position'''
         self.bg_y1 += self.bg_scroll_speed
         self.bg_y2 += self.bg_scroll_speed
@@ -66,11 +74,40 @@ class Galaga(Game):
             
         if keypress[K_DOWN]:
             self.p1_coords[1] = self.p1_coords[1] + 7
+
+        
+        
+        if self.p1_cd == 10:
+            if (keypress[K_RSHIFT] > 0 ):
+                sprite = p1.Fire(self.p1_coords)
+                if self.p1_projectile[1] < 0:
+                    self.p1_projectile[0] = self.p1_coords[0]
+                    self.p1_projectile[1] = self.p1_coords[1] + 10
+                else :
+                    self.p1_projectile[1] = self.p1_projectile[1] - 100
+             
+                
+                self.p1_cd = 0
+                win.blit(sprite,(self.p1_projectile[0],self.p1_projectile[1]))
+                pygame.display.update
             
-        if keypress[K_RSHIFT]:
-            sprite = p1.Fire()
-            win.blit(sprite,(self.p1_coords[0],self.p1_coords[1]))
+        elif self.p1_projectile[1] > 0:
+            sprite = p1.Fire(self.p1_coords)
+            if self.p1_projectile[1] < 0:
+                self.p1_projectile[0] = self.p1_coords[0]
+                self.p1_projectile[1] = self.p1_coords[1] + 10
+            else :
+                self.p1_projectile[1] = self.p1_projectile[1] - 100
+         
+            
+            self.p1_cd = 0
+            win.blit(sprite,(self.p1_projectile[0],self.p1_projectile[1]))
             pygame.display.update
+        else:
+            print(self.p1_cd)
+            self.p1_cd += 1
+        
+            
             
             
 
@@ -87,16 +124,41 @@ class Galaga(Game):
         if keypress[K_s]:
             self.p2_coords[1] = self.p2_coords[1] + 7
             
-        if keypress[K_q]:
-            sprite = p2.Fire()
-            win.blit(sprite,(self.p2_coords[0],self.p2_coords[1]))
+        if self.p2_cd == 10:
+            if (keypress[K_q] > 0 ):
+                sprite = p2.Fire(self.p2_coords)
+                if self.p2_projectile[1] < 0:
+                    self.p2_projectile[0] = self.p2_coords[0]
+                    self.p2_projectile[1] = self.p2_coords[1] + 10
+                else :
+                    self.p2_projectile[1] = self.p2_projectile[1] - 100
+             
+                
+                self.p2_cd = 0
+                win.blit(sprite,(self.p2_projectile[0],self.p2_projectile[1]))
+                pygame.display.update
+            
+        elif self.p2_projectile[1] > 0:
+            sprite = p2.Fire(self.p2_coords)
+            if self.p2_projectile[1] < 0:
+                self.p2_projectile[0] = self.p2_coords[0]
+                self.p2_projectile[1] = self.p2_coords[1] + 10
+            else :
+                self.p2_projectile[1] = self.p2_projectile[1] - 100
+         
+            
+            self.p2_cd = 0
+            win.blit(sprite,(self.p2_projectile[0],self.p2_projectile[1]))
             pygame.display.update
+        else:
+            print(self.p2_cd)
+            self.p2_cd += 1
             
 
 
         # Update sprites
-        p1_sprite = p1.Update(self.p1_coords)
-        p2_sprite = p2.Update(self.p2_coords)
+        p1_sprite = p1.Update()
+        p2_sprite = p2.Update()
         win.blit(p1_sprite,(self.p1_coords[0],self.p1_coords[1]))
         win.blit(p2_sprite,(self.p2_coords[0],self.p2_coords[1]))
         pygame.display.update
---
+++ Players.py
@@ -0,0 +1,162 @@
+from SpriteLibrary import sprite_library
+import pygame
+from pygame.locals import *
+from sheet_to_sprite import spritesheet
+from pygame_starter import Game
+
+class Player_1(sprite_library):
+    def __init__(self):
+        super().__init__()
+        self.index = 0
+       
+        # obtain predefined params
+        self.sprite = self.sprite_dir_sheet_lib[self.index]
+        self.sheet = self.sprite_dir_full_lib[self.index]
+        self.frames = self.sprite_animation_frame_lib[self.index]
+        self.sprite_size = self.df_sprite_size_lib[self.index]
+        self.sprite_max_frame = self.sprite_total_frame_lib[self.index]
+        sprite_coord = self.sprite_coord_lib[self.index]
+        self.entity_x = sprite_coord[0]
+        self.entity_y = sprite_coord[1]
+        
+        
+        self.projectile_sprite = self.sprite_dir_sheet_lib[2]
+        self.projectile_sheet = self.sprite_dir_full_lib[2]
+        self.projectile_frames = self.sprite_animation_frame_lib[2]
+        self.projectile_size = self.df_sprite_size_lib[2]
+        self.projectile_max_frame = self.sprite_total_frame_lib[2]
+        
+        projectile_coord = [self.entity_x , self.entity_y+10]
+        self.projectile_x = projectile_coord[0]
+        self.projectile_y = projectile_coord[1]
+        
+        # Key Frames  ___________
+        self.key_frames = {}
+        #                 number of frames , last frame 
+        rest_num , rest_l  = 5,5
+        fire_num , fire_l  = 1,6
+        shield_num , shield_l  = 4,10
+        dead_num , dead_l   = 8,18 
+        
+        rest_s = rest_l-rest_num
+        fire_s = fire_l-fire_num
+        shield_s = shield_l-shield_num
+        dead_s = dead_l-dead_num
+        
+        
+        self.key_frames.update({'rest start':rest_s ,'rest end':rest_l-1 , 'fire start':fire_s , 'fire end':fire_l-1 , 'shield start':shield_s,'shield end': shield_l-1 , 'dead start':dead_s , 'dead end': dead_l-1 })
+        
+       
+    def Fire(self,coords):
+        sprite = self.projectile_sprites[0]
+        return sprite 
+        
+    def GetCoords(self):
+        coords = [self.entity_x,self.entity_y]
+        print('e coord:' , coords)
+        return coords
+        
+    def GetProjectile(self):
+        coords = [self.projectile_x,self.projectile_y]
+        print('p coord:' , coords)
+        return coords
+        
+    def Update(self):
+        # Handling initilization
+        try:
+            if self.frames + 1 >= self.key_frames['rest end']:
+                self.frames = 0
+        except NameError:
+            self.frames = 0
+
+        # Scaling up sprite
+        
+        p1_sprite = pygame.transform.scale(self.p1_sprites[self.frames],(45.5,66.5))
+        #win.blit(p1_sprite,(self.entity_x,self.entity_y))
+        self.frames += 1
+
+        # Update libraries
+        self.sprite_animation_frame_lib[self.index] = self.frames
+        self.sprite_coord_lib[self.index] = [self.entity_x,self.entity_y]
+        
+        return p1_sprite
+        #pygame.display.update
+        
+
+class Player_2(sprite_library):
+    def __init__(self):
+        super().__init__()
+        self.index = 1
+      
+        # obtain predefined params
+        self.sprite = self.sprite_dir_sheet_lib[self.index]
+        self.sheet = self.sprite_dir_full_lib[self.index]
+        self.frames = self.sprite_animation_frame_lib[self.index]
+        self.sprite_size = self.df_sprite_size_lib[self.index]
+        self.sprite_max_frame = self.sprite_total_frame_lib[self.index]
+        sprite_coord = self.sprite_coord_lib[self.index]
+        self.entity_x = sprite_coord[0]
+        self.entity_y = sprite_coord[1]
+        
+        self.projectile_sprite = self.sprite_dir_sheet_lib[2]
+        self.projectile_sheet = self.sprite_dir_full_lib[2]
+        self.projectile_frames = self.sprite_animation_frame_lib[2]
+        self.projectile_size = self.df_sprite_size_lib[2]
+        self.projectile_max_frame = self.sprite_total_frame_lib[2]
+        
+        projectile_coord = [self.entity_x , self.entity_y+10]
+        self.projectile_x = projectile_coord[0]
+        self.projectile_y = projectile_coord[1]
+        
+        # Key Frames  ___________
+        self.key_frames = {}
+        #                 number of frames , last frame 
+        rest_num , rest_l  = 5,5
+        fire_num , fire_l  = 1,6
+        shield_num , shield_l  = 5,11
+        dead_num , dead_l   = 7,18 
+        
+        rest_s = rest_l-rest_num
+        fire_s = fire_l-fire_num
+        shield_s = shield_l-shield_num
+        dead_s = dead_l-dead_num
+        
+        
+        self.key_frames.update({'rest start':rest_s ,'rest end':rest_l-1 , 'fire start':fire_s , 'fire end':fire_l-1 , 'shield start':shield_s,'shield end': shield_l-1 , 'dead start':dead_s , 'dead end': dead_l-1 })
+        
+       
+    def Fire(self,coords):
+        sprite = self.projectile_sprites[1]
+        return sprite 
+        
+    def GetCoords(self):
+        coords = [self.entity_x,self.entity_y]
+        return coords
+        
+    def GetProjectile(self):
+        coords = [self.projectile_x,self.projectile_y]
+        print('p coord:' , coords)
+        return coords  
+        
+    def Update(self):
+        # Handling initilization
+        try:
+            if self.frames + 1 >= self.key_frames['rest end']:
+                self.frames = 0
+        except NameError:
+            self.frames = 0
+
+        # Scaling up sprite
+        
+        p2_sprite = pygame.transform.scale(self.p2_sprites[self.frames],(45.5,66.5))
+        #win.blit(p1_sprite,(self.entity_x,self.entity_y))
+        self.frames += 1
+
+        # Update libraries
+        self.sprite_animation_frame_lib[self.index] = self.frames
+        self.sprite_coord_lib[self.index] = [self.entity_x,self.entity_y]
+        
+        
+        
+        return p2_sprite
+        #pygame.display.update        
